0d7ba0c432356c20adc93bd0700a78b8b598495e
dnskts <dnskts@mail.ru>
2026-01-08 20:58:32 +0300

    Update main.php

--- main.php
+++ main.php
@@ -5,23 +5,23 @@ $name = 'денис';
 $patronymic = 'станиславович';
 
 // Первая буква заглавная в каждом слове
-$fullName = mb_convert_case($surname, MB_CASE_TITLE) . ' ' 
-          . mb_convert_case($name, MB_CASE_TITLE) . ' ' 
-          . mb_convert_case($patronymic, MB_CASE_TITLE);
+$fullName = mb_convert_case($surname, MB_CASE_TITLE, 'UTF-8') . ' ' 
+          . mb_convert_case($name, MB_CASE_TITLE, 'UTF-8') . ' ' 
+          . mb_convert_case($patronymic, MB_CASE_TITLE, 'UTF-8');
 
 // Аббревиатура ФИО
-$firstLetterSurname = mb_substr($surname, 0, 1);
-$firstLetterName = mb_substr($name, 0, 1);
-$firstLetterPatronymic = mb_substr($patronymic, 0, 1);
+$firstLetterSurname = mb_substr($surname, 0, 1, 'UTF-8');
+$firstLetterName = mb_substr($name, 0, 1, 'UTF-8');
+$firstLetterPatronymic = mb_substr($patronymic, 0, 1, 'UTF-8');
 
-$fio = mb_strtoupper($firstLetterSurname) 
-     . mb_strtoupper($firstLetterName) 
-     . mb_strtoupper($firstLetterPatronymic);
+$fio = mb_strtoupper($firstLetterSurname, 'UTF-8') 
+     . mb_strtoupper($firstLetterName, 'UTF-8') 
+     . mb_strtoupper($firstLetterPatronymic, 'UTF-8');
 
 // Фамилия и инициалы
-$surnameAndInitials = mb_convert_case($surname, MB_CASE_TITLE) . ' ' 
-                    . mb_strtoupper($firstLetterName) . '.' 
-                    . mb_strtoupper($firstLetterPatronymic) . '.';
+$surnameAndInitials = mb_convert_case($surname, MB_CASE_TITLE, 'UTF-8') . ' ' 
+                    . mb_strtoupper($firstLetterName, 'UTF-8') . '.' 
+                    . mb_strtoupper($firstLetterPatronymic, 'UTF-8') . '.';
 
 echo "Полное имя: '$fullName'\n";
 echo "Фамилия и инициалы: '$surnameAndInitials'\n";
